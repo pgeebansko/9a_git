2220b97eb7949e073c6087c6b20618f0fdfaead8
pgeebansko <pgee_students@abv.bg>
2023-03-02 10:19:47 +0200

    точка 4 на нашия проект

--- git_start.py
+++ git_start.py
@@ -4,7 +4,7 @@ import func
 win = tk.Tk()
 win.title(' Суперски проект')
 win.geometry('800x600')
-win.configure(bg='red')
+win.configure(bg='blue')
 btn = tk.Button(win, text='Click me!', command=func.my_function)
 btn.pack()
 win.mainloop()
